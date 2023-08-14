7ff544504894907f61107ba68908b023382f90cc
KPInc425 <kpinc425@gmail.com>
2023-08-13 23:37:50 -0700

    Add ability to request public games, add authization checks to all games, add game demo page and component, adjust game component styling

--- src/+Modules/Lazy/WordSearchKingdom/+Api/+WskApi/src/Controllers/GamesController.cs
+++ src/+Modules/Lazy/WordSearchKingdom/+Api/+WskApi/src/Controllers/GamesController.cs
@@ -6,14 +6,19 @@ public partial class GamesController : BaseController
     {
         // alternatively
         var claimsIdentity = HttpContext.User.Identity as ClaimsIdentity;
-        Guid? userId = new Guid(claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier)?.Value);
-        
-        if (userId.HasValue)
-        {
-            request.KnownUserId = userId.Value;
+        if (claimsIdentity.IsAuthenticated == true ) {
+            Guid? userId = new Guid(claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+            if (userId.HasValue)
+            {
+                request.KnownUserId = userId.Value;
+                var response = await _dataService.GamesGetAllAsync(request);
+                return Ok(response);                            
+            } 
+
+        } else {
             var response = await _dataService.GamesGetAllAsync(request);
             return Ok(response);                            
-        }        
+        }     
         return Ok(null);
     }
 }
